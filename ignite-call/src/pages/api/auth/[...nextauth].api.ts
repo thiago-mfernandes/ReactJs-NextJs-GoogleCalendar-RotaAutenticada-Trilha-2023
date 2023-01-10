3a23dbc6efa2f5e9137f9a341430bb7894450a1a
Thiago Fernandes <falecomobranco@yahoo.com.br>
2023-01-10 17:32:56 -0300

    coding

--- ignite-call/src/pages/api/auth/[...nextauth].api.ts
+++ ignite-call/src/pages/api/auth/[...nextauth].api.ts
@@ -1,7 +1,10 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "../../../lib/auth/prisma-adapter"
 
 export const authOptions: NextAuthOptions = {
+  // o prisma adapater contem todos os metodos para a persistencia de dados no BD
+  adapter: PrismaAdapter(),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
